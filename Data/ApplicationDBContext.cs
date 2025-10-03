9ff9eedbd474abc7c3228e9164e698aa688a2979
devgit2025 <github2025.01@gmail.com>
2025-10-03 19:28:33 +0700

    add Model

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,6 +1,20 @@
-﻿namespace food_asp.Data
+﻿using Microsoft.EntityFrameworkCore;
+using food_asp.Models;
+namespace food_asp.Data
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext:DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> tb_Customers { get; set; }
+
+        public DbSet<Product> tb_Product { get; set; }
+
+        public DbSet<Admin> tb_Admin { get; set; }
+
+        public DbSet<Order> tb_Order { get; set; }
+
+        public DbSet<OrderDetail> tb_OrderDetail { get; set; }
     }
 }
